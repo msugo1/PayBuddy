212cfbcb40a44c091e7d787b520d7307848997f9
soo <soo@soos-MacBook-Pro.local>
2025-12-14 21:33:11 +0900

    프로젝트 생성 (Spring Initializr)

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.8"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.asciidoctor.jvm.convert") version "4.0.5"
+}
+
+group = "co.paybuddy"
+version = "0.0.1-SNAPSHOT"
+description = "Payment Platform"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+	inputs.dir(project.extra["snippetsDir"]!!)
+	dependsOn(tasks.test)
+}
---
+++ src/test/kotlin/co/paybuddy/payment/PaymentApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.paybuddy.payment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
