99f53cb31c72c74f2b0c63b6d642b6fab92737d5
soo <gomsu1045@gmail.com>
2025-12-18 12:44:09 +0900

    fix(test): CardPaymentMethod 필수 필드 누락 수정
    - card_holder_name, installment_months 필드 추가
    - 스키마 변경(410c5e8)에 따른 테스트 업데이트

--- payment/src/test/kotlin/com/paybuddy/payment/api/PaymentsApiTest.kt
+++ payment/src/test/kotlin/com/paybuddy/payment/api/PaymentsApiTest.kt
@@ -168,7 +168,9 @@ class PaymentsApiTest {
                                     "card_number": "1234567890123456",
                                     "expiry_month": "12",
                                     "expiry_year": "2025",
-                                    "cvc": "123"
+                                    "cvc": "123",
+                                    "card_holder_name": "홍길동",
+                                    "installment_months": 0
                                 }
                             }
                         }
