a8fea818328ef0772e17dad4ef60a296d4d1015f
soo <gomsu1045@gmail.com>
2025-12-23 17:20:57 +0900

    review: spring 공통 convention 내 boot 플러그인 제거 (bootstrap 외 불필요)

--- buildSrc/src/main/kotlin/paybuddy.spring-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/paybuddy.spring-conventions.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
 	id("paybuddy.kotlin-conventions")
-	id("org.springframework.boot")
 	id("io.spring.dependency-management")
 	kotlin("plugin.spring")
 }
@@ -10,8 +9,4 @@ dependencies {
 
 	testImplementation(libs.spring.boot.starter.test)
 	testRuntimeOnly(libs.junit.platform.launcher)
-}
-
-tasks.jar {
-	enabled = true
 }
--- payment/build.gradle.kts
+++ payment/build.gradle.kts
@@ -1,16 +1,9 @@
 plugins {
     id("paybuddy.spring-boot-conventions")
+	id("org.springframework.boot")
 }
 
 dependencies {
     implementation(libs.spring.boot.starter.web)
     implementation(libs.jackson.module.kotlin)
-}
-
-tasks.bootJar {
-    enabled = true
-}
-
-tasks.jar {
-    enabled = false
 }
